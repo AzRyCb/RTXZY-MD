d6cc606421f338e42f9bbf75303795c5671a8b66
AzRyCb <azry695@gmail.com>
2025-01-05 14:55:07 +0700

    update config.js

--- config.js
+++ config.js
@@ -1,28 +1,28 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6283879175089']  
+global.mods = ['6283879175089'] 
+global.prems = ['6283879175089']
+global.nameowner = 'Owner Neubot'
+global.numberowner = '62882001552691' 
+global.mail = '' 
+global.gc = 'https://chat.whatsapp.com/FV3MU8D22P19dKEyA7EAfD'
+global.instagram = 'https://instagram.com/z_ryyw'
+global.wm = '© NeuBot'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made With'
-global.author = 'Bot WhatsApp'
+global.author = 'Neubot'
 global.maxwarn = '3' // Peringatan maksimum Warn
 
-global.autobio = false // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
+global.autobio = true // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
 global.antiporn = true // Set true/false untuk Auto delete pesan porno (bot harus admin) (default: true)
 global.spam = true // Set true/false untuk anti spam (default: true)
-global.gcspam = false // Set true/false untuk menutup grup ketika spam (default: false)
+global.gcspam = true // Set true/false untuk menutup grup ketika spam (default: false)
     
 
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'Mna7Xxvn' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
